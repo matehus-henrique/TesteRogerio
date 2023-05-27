084a8a9fe802d819bf3cca267a4353b7ddff49a3
matehus-henrique <mtheusriki@gmail.com>
2023-05-27 11:20:47 -0300

    Criação do banco de dados para o perfil

---
+++ database/migrations/2023_05_27_141019_create_perfis_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePerfisTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('perfis', function (Blueprint $table) {
+            $table->id();
+            $table->string('nome');
+            $table->string('sobrenome');
+            $table->date('dbo');
+            $table->string('genero');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('perfis');
+    }
+}
