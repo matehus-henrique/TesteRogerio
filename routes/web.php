171d96671cee07990d4c6a6cce0fb0e85d930089
matehus-henrique <mtheusriki@gmail.com>
2023-05-27 17:16:51 -0300

    Criação das migrations

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,14 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PerfilController;
 
-Route::resource('perfis', PerfilController::class);
+
+Route::get('perfis', [PerfilController::class, 'index']);
+Route::get('perfis/create', [PerfilController::class, 'create']);
+Route::post('perfis', [PerfilController::class, 'store'])->name('perfis.store');
+Route::get('perfis/{perfil}', [PerfilController::class, 'show'])->name('perfis.show');
+Route::get('perfis/{perfil}/edit', [PerfilController::class, 'edit'])->name('perfis.edit');
+Route::put('perfis/{perfil}', [PerfilController::class, 'update'])->name('perfis.update');
+Route::delete('perfis/{perfil}', [PerfilController::class, 'destroy'])->name('perfis.destroy');
 
 Route::get('/', function () {
     return view('welcome');
